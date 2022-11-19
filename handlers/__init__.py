e82ebeb567bd49759f23f3861eaaccf7127fa2bf
Amersant1 <96844840+Amersant1@users.noreply.github.com>
2022-11-19 13:44:50 +0300

    work

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,6 +2,6 @@ from . import managers
 from . import sub_handlers
 from . import friends_horoscope
 from . import sending_horoscope
-from . import general
 from . import static_answers
 from . import registration
+from . import general
