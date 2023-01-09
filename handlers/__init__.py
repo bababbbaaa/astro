11376c0eeb2373e9b5a3ad92b70ea4925a3315d2
Amersant1 <96844840+Amersant1@users.noreply.github.com>
2023-01-09 17:30:10 +0300

    commit

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,6 @@
 from . import reg
 from . import sub_handlers
-from . import friends_horoscope
+# from . import friends_horoscope
 from . import sending_horoscope
 from . import managers
 from . import generate_token
